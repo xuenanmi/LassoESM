6b3943042fa0d88f68d04e9fc03d30242adf6179
xuenanmi <80510335+xuenanmi@users.noreply.github.com>
2024-09-06 14:35:03 -0500

    Update pred_non_natural_cyclase_peptide_pairs.py

--- code/cyclase_peptide_pair_prediction/pred_non_natural_cyclase_peptide_pairs.py
+++ code/cyclase_peptide_pair_prediction/pred_non_natural_cyclase_peptide_pairs.py
@@ -200,7 +200,7 @@ if __name__ == "__main__":
 
 
     # Load training/validation data
-    train_val_data = pd.read_csv('Cyclase_substrate_pairs_pos_neg_with_filter.csv') #FusC and other natural lasso peptide pairs (with acceptor residue E)
+    train_val_data = pd.read_csv('Cyclase_substrate_pairs_pos_neg_with_filter.csv') 
     Cyclase_seq_train_val = train_val_data.iloc[:, 0].tolist()
     substrate_seq_train_val = train_val_data.iloc[:, 1].tolist()
     labels_train_val = train_val_data.iloc[:, 2].tolist()
@@ -209,7 +209,7 @@ if __name__ == "__main__":
     max_substrate_length = max(len(seq) for seq in substrate_seq_train_val) + 2    
 
     # Load test data
-    test_data = pd.read_csv('test_set_non_natural_Fus_with_filter.csv')
+    test_data = pd.read_csv('test_set_non_natural_Fus_with_filter.csv') #FusC and other natural lasso peptide pairs (with acceptor residue E)
     Cyclase_seq_test = test_data.iloc[:, 0].tolist()
     substrate_seq_test = test_data.iloc[:, 1].tolist()
 
