afa1653fb5a19d65ded89616b4e5f6a8bd5adc08
xuenanmi <80510335+xuenanmi@users.noreply.github.com>
2024-11-13 21:29:24 -0600

    Update downstream_models_performance_diff_4embs.py

--- code/substrate_tolerance_prediction/downstream_models_performance_diff_3embs.py
+++ code/substrate_tolerance_prediction/downstream_models_performance_diff_3embs.py
@@ -13,99 +13,65 @@ import matplotlib.pyplot as plt
 from scipy.stats import sem
 import scipy.stats as stats
 
-def cv_res(random_seed,Xs, ys, RF_estimators, RF_depth, RF_features, Ada_estimators, Ada_lr, SVC_C, SVC_kernel, MLP_hidden_layers, MLP_batch, MLP_lr):
-    """
-    Perform cross-validation for multiple classifiers and return their balanced accuracy scores.
+
+def cv_res(Xs, ys, RF_estimators, RF_depth, RF_features, Ada_estimators, Ada_lr, SVC_C, SVC_kernel, MLP_hidden_layers, MLP_batch, MLP_lr):
+    random_seed = 42  
     
-    Args:
-        random_seed (int): Random seed for reproducibility.
-        Xs (np.ndarray): Input features.
-        ys (list): Target labels.
-        RF_estimators (int): Number of estimators for RandomForest.
-        RF_depth (int): Maximum depth for RandomForest.
-        RF_features (str): Max features for RandomForest.
-        Ada_estimators (int): Number of estimators for AdaBoost.
-        Ada_lr (float): Learning rate for AdaBoost.
-        SVC_C (float): Regularization parameter for SVC.
-        SVC_kernel (str): Kernel type for SVC.
-        MLP_hidden_layers (tuple): Hidden layer sizes for MLPClassifier.
-        MLP_batch (int): Batch size for MLPClassifier.
-        MLP_lr (float): Learning rate for MLPClassifier.
-    
-    Returns:
-        list: Cross-validation results (balanced accuracy scores) for each classifier.
-    """
-    MLP = MLPClassifier(hidden_layer_sizes= MLP_hidden_layers, batch_size = MLP_batch, learning_rate_init = MLP_lr, early_stopping=True, max_iter=1000, random_state = random_seed)
-    RF = RandomForestClassifier(n_estimators = RF_estimators, max_depth = RF_depth, max_features = RF_features,random_state = random_seed)
-    Ada = AdaBoostClassifier(n_estimators = Ada_estimators, learning_rate = Ada_lr ,random_state = random_seed)
-    SVM = SVC(C = SVC_C, kernel = SVC_kernel,random_state = random_seed)
-    # Perform cross-validation
-    cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=random_seed)
-    MLP_CV = cross_val_score(MLP, Xs, ys, cv = cv, n_jobs = -1, scoring = 'balanced_accuracy')
-    pca = PCA(n_components = 100) 
-    Xs_pca = pca.fit_transform(Xs)
-    RF_CV = cross_val_score(RF, Xs_pca, ys, cv = cv, n_jobs = -1, scoring = 'balanced_accuracy')
-    Ada_CV = cross_val_score(Ada, Xs_pca, ys, cv = cv, n_jobs = -1, scoring = 'balanced_accuracy')
-    SVC_CV = cross_val_score(SVM, Xs_pca, ys, cv = cv, n_jobs = -1, scoring = 'balanced_accuracy')
-    
-    cv_res = [np.mean(KNN_CV), np.mean(RF_CV), np.mean(Ada_CV), np.mean(SVC_CV), np.mean(MLP_CV)]
+    RF = RandomForestClassifier(n_estimators=RF_estimators, max_depth=RF_depth, max_features=RF_features, random_state=random_seed)
+    Ada = AdaBoostClassifier(n_estimators=Ada_estimators, learning_rate=Ada_lr, random_state=random_seed)
+    SVM = SVC(C=SVC_C, kernel=SVC_kernel, random_state=random_seed)
+    MLP = MLPClassifier(hidden_layer_sizes=MLP_hidden_layers, batch_size=MLP_batch, learning_rate_init=MLP_lr, early_stopping=True, max_iter=1000, random_state=random_seed)
+
+    cv = RepeatedKFold(n_splits=10, n_repeats=5, random_state=random_seed)
+    MLP_CV = cross_val_score(MLP, Xs, ys, cv=cv, n_jobs=-1, scoring='balanced_accuracy')
+    RF_CV = cross_val_score(RF, Xs, ys, cv=cv, n_jobs=-1, scoring='balanced_accuracy')
+    Ada_CV = cross_val_score(Ada, Xs, ys, cv=cv, n_jobs=-1, scoring='balanced_accuracy')
+    SVC_CV = cross_val_score(SVM, Xs, ys, cv=cv, n_jobs=-1, scoring='balanced_accuracy')
+
+    cv_res = {
+        'RF': np.mean(RF_CV.reshape(-1, 10), axis=1),
+        'Ada': np.mean(Ada_CV.reshape(-1, 10), axis=1),
+        'SVC': np.mean(SVC_CV.reshape(-1, 10), axis=1),
+        'MLP': np.mean(MLP_CV.reshape(-1, 10), axis=1)
+    }
+    #print(cv_res)
     return cv_res
 
-if __name__ == '__main__': 
-   random_seed_ls = random.sample(range(100), 3)
-   # Load data
-   # Vanilla ESM    
-   vanilla_Xs = np.load('../data/vanilla_ESM_embs.npy')
-   data = pd.read_csv('../data/FusA_tolerance_dataset.csv')
-   ys = data.iloc[:,1].tolist()
-   # Peptide ESM 
-   Peptide_Xs = np.load('../data/FusA_embs_from_PeptideESM_650M.npy')
-   # Lasso ESM
-   rodeo_Xs = np.load('../data/FusA_embs_from_RODEO_ESM_650M_lr_5e-05_batch_size_8.npy')
-   
 
-   vanilla_ESM_cv_res, Peptide_ESM_cv_res, rodeo_ESM_cv_res = [], [],[]
+if __name__ == '__main__':
+    # Load existing data and embeddings
+    one_hot_Xs = np.load('FusA_one_hot_encoding.npy')
+    vanilla_Xs = np.load('vanilla_ESM_embs.npy')
+    Peptide_Xs = np.load('FusA_embs_from_PeptideESM_650M.npy')
+    rodeo_Xs = np.load('FusA_embs_from_RODEO_ESM_650M_lr_5e-05_batch_size_8.npy')
 
-   for i in random_seed_ls:
-       vanilla_ESM_cv_res.append(cv_res(i, vanilla_Xs, ys, 100, 50, 'sqrt', 200, 0.1, 10, 'linear', (512,64), 16, 0.01))   #utilize the optimized hyperparameters
-       Peptide_ESM_cv_res.append(cv_res(i, Peptide_Xs, ys, 100, 10, 'sqrt', 200, 0.1, 10, 'linear', (256,32), 16, 0.001))
-       rodeo_ESM_cv_res.append(cv_res(i, rodeo_Xs, ys,     200, 20, 'sqrt', 200, 0.1, 10, 'linear', 256, 16, 0.001))
-       
-   vanilla_ESM = pd.DataFrame(vanilla_ESM_cv_res, columns = ['RF','Ada','SVC','MLP'])
-   Peptide_ESM = pd.DataFrame(Peptide_ESM_cv_res, columns = ['RF','Ada','SVC','MLP'])
-   RODEO_ESM = pd.DataFrame(rodeo_ESM_cv_res, columns = ['RF','Ada','SVC','MLP'])
-   
-   vanilla_ESM['Model'] = 'Vanilla_ESM'
-   Peptide_ESM['Model'] = 'Peptide_ESM'
-   RODEO_ESM['Model'] = 'Lasso_ESM'
-   
-   result = pd.concat([vanilla_ESM, Peptide_ESM, RODEO_ESM], ignore_index=True) 
-   print(result)
-   # Process the dataframe and prepare for plotting
-   model = result['Model'].tolist()
-   reshape_df = pd.DataFrame(result.iloc[:,:4].values.ravel('F'))
-   reshape_df.columns = ['BA']
-   reshape_df['Downstream_model'] = ['RF'] * 9 + ['AdaBoost'] * 9 + ['SVC'] * 9 + ['MLP'] * 9
-   reshape_df['Embeddings'] = 4*model
-   # Plotting
-   f, ax = plt.subplots(ncols=1, nrows=1, figsize=(10,7))
-   flatui = ["#82ee98","#f7df94","#f493cf"]
-   sns.set_palette(sns.color_palette(flatui))
-   aa = sns.barplot(x="Downstream_model", y="BA",hue="Embeddings",data=reshape_df, alpha = 1.0,capsize=.1,errorbar=('ci', 68), errwidth=1, linewidth = 1) #, edgecolor=1)
-   for patch in aa.patches:
-       clr = patch.get_facecolor()
-       patch.set_edgecolor(clr)
-   for spine in ['bottom', 'left', 'top', 'right']:
-       ax.spines[spine].set_linewidth(2)
-   plt.ylim(0.3,1.0)
-   plt.xticks(fontsize=18)
-   plt.yticks(fontsize=18)
-   plt.xlabel('Downstream Models', fontsize = 20)
-   plt.ylabel('Balanced Accuracy', fontsize = 20)
-   #ax.get_legend().remove()
-   plt.savefig("Diff_3embs_cv_BA_res.png", dpi =300) 
-   # Statistical tests 
-   for i in range(4):
-       print(result.columns[i])
-       print(stats.ttest_ind(result.iloc[:3,i], result.iloc[3:6,i], equal_var = False))
-       print(stats.ttest_ind(result.iloc[:3,i], result.iloc[6:9,i], equal_var = False)) 
+    data = pd.read_excel('231130_FusA_Mutants_SEBedit.xlsx')
+    ys = data.iloc[:, 1].tolist()
+     
+    # Call cv_res and get results for each embedding type
+    res_one_hot = cv_res(one_hot_Xs, ys, 100, 20, 'log2', 100, 1, 1, 'rbf', 64, 64, 0.01)
+    print('Done one_hot')
+    res_vanilla = cv_res(vanilla_Xs, ys, 100, 50, 'sqrt', 200, 0.1, 10, 'linear', (512, 64), 16, 0.01)
+    print('Done')
+    res_peptide = cv_res(Peptide_Xs, ys, 100, 10, 'sqrt', 200, 0.1, 10, 'linear', (256, 32), 16, 0.001)
+    print('Done')
+    res_rodeo = cv_res(rodeo_Xs, ys, 200, 20, 'sqrt', 200, 0.1, 10, 'linear', 256, 16, 0.001)
+    print('Done')
+
+    # Create dataframes for each embedding type
+    one_hot = pd.DataFrame(res_one_hot)
+    vanilla_ESM = pd.DataFrame(res_vanilla)
+    Peptide_ESM = pd.DataFrame(res_peptide)
+    rodeo_ESM = pd.DataFrame(res_rodeo)
+
+    # Add a column for each embedding type
+    one_hot['Model'] = 'One hot'
+    vanilla_ESM['Model'] = 'Vanilla_ESM'
+    Peptide_ESM['Model'] = 'Peptide_ESM'
+    rodeo_ESM['Model'] = 'Lasso_ESM'
+
+    # Concatenate all results
+    result = pd.concat([one_hot, vanilla_ESM, Peptide_ESM, rodeo_ESM], ignore_index=True) 
+    print(result)
+    result.to_csv('diff_4_embs_cv_BA_res.csv', index=False)
+    
